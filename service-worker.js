e34cff3459d9bd604a86aab2a866f153f20b6499
anwarbio <bioanwar@gmail.com>
2026-05-18 12:49:54 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -73,28 +73,3 @@ self.addEventListener("fetch", event => {
   );
 });
 
-self.addEventListener("activate", event => {
-
-  event.waitUntil(
-
-    caches.keys().then(keys => {
-
-      return Promise.all(
-
-        keys.map(key => {
-
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-
-        })
-
-      );
-
-    })
-
-  );
-
-  self.clients.claim();
-
-});
